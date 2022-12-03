aa1800d52291c4ce7209a8cc8d2af700c093abb6
ThiagoMonts <thiagohonorato@hotmail.com>
2022-12-03 12:07:05 -0300

    Segundo dia de prática

--- treinamentoMinsait/cypress/e2e/primeiroTeste.cy.js
+++ treinamentoMinsait/cypress/e2e/primeiroTeste.cy.js
@@ -1,13 +1,21 @@
 
 /// <reference types="cypress" />
 describe("Minha primeira suite de testes.", () => {
-
-    it("Validar acesso a URL", () => {
-        cy.visit("https://www.saucedemo.com/")
+    beforeEach(()=>{
+        cy.visit("/")
+        cy.get("#user-name").type("standard_user")
+        cy.get("#password").type("secret_sauce")
+        cy.get("#login-button").click()
     })
 
     it("Validar login com sucesso", () => {
-        cy.visit("https://www.saucedemo.com/")
-        cy.get("#user-name").type("standard_user")
+        cy.get(".app_logo").should("be.visible")
     })
+
+    it("Validar adicionar produto no carrinho", () => {
+        cy.get("#add-to-cart-sauce-labs-backpack").click()
+        cy.get("#remove-sauce-labs-backpack").should("have.text", "Remove")
+        cy.get(".shopping_cart_badge").should("be.visible")
+    })
+
 })
