b7de2f1b84e7942999754accc6116abeb7d082a4
ThiagoMonts <thiagohonorato@hotmail.com>
2022-12-09 22:11:24 -0300

    Update Test

--- exercicioMinsait/cypress/e2e/exercicio.cy.js
+++ exercicioMinsait/cypress/e2e/exercicio.cy.js
@@ -1,16 +1,44 @@
+/// <reference types="cypress" />
+
 describe('exercicio em sala de aula', () => {
   beforeEach(() => {
-    cy.visit("https://demo.automationtesting.in/Register.html")
-    cy.get("input[ng-model='FirstName']").type("Thiago")
-    cy.get("input[ng-model='LastName']").type("Silva")
-    cy.get("input[ng-model='EmailAdress']").type("google@google.com")
-    cy.get("input[ng-model='Phone']").type("0123456789")
-    cy.get("input[ng-model='radiovalue']").check("Male")
-    //cy.get("input[ng-model='country']").type("Select Country")
-    cy.get("#submitbtn").click()
+    cy.visit('https://demo.automationtesting.in/Register.html')
   })
 
-  it('Validar envio do formulário', () => {
-    cy.get('#submitbtn').should('not.be.visible')
+  it('Preencher campos do formulário', () => {
+    var user = {
+      firstName: "Thiago",
+      lastName: "Silva",
+      adress: "Avenida Epitácio Pessoa, 100",
+      email: "google@google.com",
+      phoneNumber: "0123456789",
+      password: "password",
+      confirmPassword: "password",
+      btnSubmit: "Submit",
+    }
+
+    cy.get("input[ng-model='FirstName']").type(user.firstName)
+    cy.get("input[ng-model='LastName']").type(user.lastName)
+    cy.get("textarea[ng-model='Adress']").type(user.adress)
+    cy.get("input[ng-model='EmailAdress']").type(user.email)
+    cy.get("input[ng-model='Phone']").type(user.phoneNumber)
+    cy.get("input[ng-model='radiovalue']").check("Male")
+    cy.get("#checkbox1").click()
+    cy.get("#msdd").click()
+    cy.get(":nth-child(1) > .ui-corner-all").click()
+    cy.get(":nth-child(30) > .ui-corner-all").click()
+    cy.get("#header .row h1").click()
+    cy.get("#Skills").select("Android")
+    cy.get("#countries")
+    cy.get(".select2-selection").click()
+    cy.get("#select2-country-results > :nth-child(11)").click()
+    cy.get("#yearbox").select("1986")
+    cy.get("select[ng-model='monthbox']").select("June")
+    cy.get("#daybox").select("18")
+    cy.get("#firstpassword").type(user.password)
+    cy.get("#secondpassword").type(user.confirmPassword)
+
+    cy.contains("#submitbtn", user.btnSubmit).click()
   })
+
 })
